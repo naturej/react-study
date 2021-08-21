370c1904a65cb0dba5ceb2eb89ada8a6ec065d64
정자연 <nature@naturej-MacBookPro.local>
2021-08-21 14:33:05 +0900

    favorite 배열 보여주기

--- src/pages/Favorite.js
+++ src/pages/Favorite.js
@@ -1,8 +1,9 @@
 const Favorite = () => {
+  const favorites = localStorage.getItem('favorites');
   return (
     <main>
       <h1>Favorite Page</h1>
-      
+      favorites : {favorites}
     </main>
   );
 };
